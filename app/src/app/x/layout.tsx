f9e2f1d9c83affe6312c889a26362c91d9567f8d
Atanas Krondev <krondev@gmail.com>
2024-01-08 12:26:10 +0200

    CHORE: X page footer flowchart

---
+++ app/src/app/x/layout.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function XLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <footer className="container px-6 py-10 md:px-10">
+        <h2 className="text-2xl font-medium">How It Works</h2>
+        <Image
+          src="/how-it-works-x.jpg"
+          width={1926}
+          height={1318}
+          alt="how-it-works"
+          className="mt-6 rounded-lg border border-border"
+        />
+      </footer>
+    </>
+  )
+}
